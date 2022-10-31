53ec4fab41f6ed2a09f827bd8872d69a3ff6c5ad
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-10-31 15:05:41 +0900

    Initial commit

---
+++ src/test/java/com/example/mymvc/MyMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/example/mymvc/PasswordValidator.java
@@ -0,0 +1,42 @@
+package com.example.mymvc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+/**
+ * 패스워드는 최소 8자리 이상 12자리 이하여야한다.
+ * 비밀번호가 8자 미만, 12자 초과일 경우 IllegalArgumentException 을 발생시켜야 한다.
+ * 검색 조건에 대해 테스트 코드를 작성해야 한다.
+ */
+public class PasswordValidator {
+
+    public final static String WRONG_PASSWORD_LEGNTH_EXCEPTION_MESSAGE = "비밀번호가 최소 8자 이상 12자 이하여야 합니다.";
+
+    @DisplayName("비밀번호가 최소 8자이상 12자 이하일 경우 오류가 발생하지 않음")
+    @Test
+    void validatePasswordTest() {
+          assertThatCode(() -> PasswordValidator.validate("serverWizard"))
+                  .doesNotThrowAnyException ();
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과일때 IllegalArgumentException 에러가 발생한다.")
+    @Test
+    void validatePasswordTest2() {
+        assertThatCode(()-> PasswordValidator.validate2("serverWizard"))
+                .doesNotThrowAnyException();
+    }
+
+    private static void validate(String password) {
+        int length = password.length();
+
+        if (length < 8 || length > 12) {
+            throw new IllegalArgumentException(WRONG_PASSWORD_LEGNTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static void validate2(String password) {
+
+    }
+}
